4d0fd1c109d575db05b4726c2f7a4d00ff746352
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-10-22 15:34:45 +0300

    Creaate the sidebar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,27 +11,29 @@ import { useUserContext } from "../context/user_context";
 const Nav = () => {
   return (
     <NavContainer>
-      <div className="nav-center nav-header">
-        <div>
+      <div className="nav-center">
+        <div className="nav-header">
           <Link to="/">
             <img src={logo} alt="comfy sloth" />
           </Link>
+
+          <button className="nav-toggle" type="button">
+            <FaBars />
+          </button>
         </div>
-        <div>
-          <ul className="nav-links">
-            {links.map((link) => {
-              const { id, text, url } = link;
-              return (
-                <li key={id}>
-                  <Link to={url}>{text}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div>
-          <CartButtons />
-        </div>
+
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+
+        <CartButtons />
       </div>
     </NavContainer>
   );
